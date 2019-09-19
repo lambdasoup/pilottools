45e1567d9e475e495409fa41a7926d03a1b24db9
Maximilian Hille <mh@lambdasoup.com>
2019-09-19 21:32:32 +0200

    parse beacon

--- xpnet/src/main/java/com/lambdasoup/xpnet/XpNet.kt
+++ xpnet/src/main/java/com/lambdasoup/xpnet/XpNet.kt
@@ -1,8 +1,7 @@
 package com.lambdasoup.xpnet
 
-import java.io.IOException
+import java.io.*
 import java.net.DatagramPacket
-import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.MulticastSocket
 
@@ -16,9 +15,40 @@ fun main() {
             val packet = DatagramPacket(message, message.size)
             print("UDP client: about to wait to receive")
             udpSocket.receive(packet)
-            val text = String(message, 0, packet.getLength())
-            print("Received data $text")
+            val text = String(message, 0, packet.length)
+            val bis = DataInputStream(ByteArrayInputStream(message))
+            val ba = ByteArray(5)
+            val prologue = bis.read(ba)
+            print("Received data ${String(ba)}")
+
+            val beacon = Beacon(
+                majorVersion = bis.readUnsignedByte(),
+                minorVersion = bis.readUnsignedByte(),
+                applicationHostId = bis.readInt(),
+                versionNumber = bis.readInt(),
+                role = bis.readUnsignedInt(),
+                port = bis.readUnsignedShort(),
+                computerName = run {
+                    val ba = ByteArray(500)
+                    bis.read(ba)
+                    String(ba).trim()
+                }
+            )
+            print("Received data $beacon")
         } catch (e: IOException) {
             print("UDP client has IOException/error: $e")
         }
 }
+
+data class Beacon(
+    val majorVersion: Int,
+    val minorVersion: Int,
+    val applicationHostId: Int,
+    val versionNumber: Int,
+    val role: Long,
+    val port: Int,
+    val computerName: String
+)
+
+fun DataInputStream.readUnsignedInt(): Long = this.readInt().toUnsignedLong()
+fun Int.toUnsignedLong(): Long = Integer.toUnsignedLong(this)
