94bd028c865318b66d3ca74157f9b69db6057cd5
Maximilian Hille <mh@lambdasoup.com>
2019-10-13 12:00:40 +0200

    create connection class

--- xpnet/src/main/java/com/lambdasoup/xpnet/XpNet.kt
+++ xpnet/src/main/java/com/lambdasoup/xpnet/XpNet.kt
@@ -1,34 +1,59 @@
 package com.lambdasoup.xpnet
 
-import java.io.DataInputStream
-import java.io.IOException
 import java.net.DatagramPacket
+import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.MulticastSocket
+import java.nio.ByteBuffer
 
 fun main() {
-    try {
-        val udpSocket = MulticastSocket(49707)
-        udpSocket.reuseAddress = true
-        val group = InetAddress.getByName("239.255.1.1")
-        udpSocket.joinGroup(group)
-        val ba = ByteArray(1500)
-        val packet = DatagramPacket(ba, ba.size)
-        udpSocket.receive(packet)
-        println("received packet from ${packet.address} with length ${packet.length}")
+    val client = getClient()
 
-        val msg = Message.wrap(ba, packet.length)
+    val connection = Connection(client)
 
-        println(msg.base64String())
+    connection.sendCommand("sim/electrical/battery_1_toggle")
+}
 
+class Connection(private val client: Client) {
 
-        val beacon = Parser().parse(msg)
+    private val socket = DatagramSocket()
+    private val sendBuf = ByteBuffer.allocate(500)
 
-        println("Received data $beacon")
-    } catch (e: IOException) {
-        println("UDP client has IOException/error: $e")
+    fun sendCommand(command: String) {
+        sendBuf.rewind()
+        val ba = "CMND\u0000$command\u0000".toByteArray(charset = Charsets.US_ASCII)
+        val packet = DatagramPacket(ba, 0, ba.size, client.address, 49000)
+        socket.send(packet)
     }
 }
+
+
+data class Client(
+    val name: String,
+    val address: InetAddress
+)
+
+fun getClient(): Client {
+    val udpSocket = MulticastSocket(49707)
+    udpSocket.reuseAddress = true
+    val group = InetAddress.getByName("239.255.1.1")
+    udpSocket.joinGroup(group)
+    val ba = ByteArray(1500)
+    val packet = DatagramPacket(ba, ba.size)
+    udpSocket.receive(packet)
+    val msg = Message.wrap(ba, packet.length)
+
+
+    val beacon = Parser().parse(msg)
+    return Client(
+        name = beacon.computerName,
+        address = packet.address
+    )
+}
+
+data class Command(
+    val commandStr: String
+)
 
 data class Beacon(
     val majorVersion: Byte,
@@ -39,6 +64,3 @@ data class Beacon(
     val port: UShort,
     val computerName: String
 )
-
-fun DataInputStream.readUnsignedInt(): Long = this.readInt().toUnsignedLong()
-fun Int.toUnsignedLong(): Long = Integer.toUnsignedLong(this)
